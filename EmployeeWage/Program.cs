a2d5ef34d921043cf5437c95696065572802018f
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-01 12:38:03 +0530

    Added: Daily wage of employee.

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,17 +2,31 @@
 
 class Employee
 {
-    public string attendanceCheck()
+    //constants
+    int WAGE_PER_HOUR = 20;
+    int FULLDAY_HOUR = 8;
+    int ABSENTDAY_HOUR = 0;
+
+    //Variables
+    int wagePerDay = 0;
+
+    public int attendanceCheck()
     {
+
         Random random = new Random();
         int attendanceRandom = random.Next(2);
+        Console.WriteLine(attendanceRandom);
         if(attendanceRandom==1)
         {
-            return "Emp Present";
+            Console.WriteLine("Emp Present");
+            wagePerDay = WAGE_PER_HOUR * FULLDAY_HOUR;
+            return wagePerDay;
         }
         else
         {
-            return "Emp Absent";
+            Console.WriteLine("Emp Absent");
+            wagePerDay = WAGE_PER_HOUR * ABSENTDAY_HOUR;
+            return wagePerDay;
         }
     }
 }
@@ -21,7 +35,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        String checkAttendance;
+        int checkAttendance;
         Console.WriteLine("Welcome to Employee Wage Program!");
         Employee e1 = new Employee();
         checkAttendance=e1.attendanceCheck();
